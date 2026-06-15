e4fbede81bc011a71d57d676cf84a27e9996eb2e
Евгений Колпаков <pompei@mail.ru>
2026-06-15 15:10:45 +0500

    Created test db

---
+++ docker/pg-init-test/init.sql
@@ -0,0 +1,3 @@
+CREATE USER probe_lang_test_user_01 WITH PASSWORD 'MHVYBQ2u2uJgmPCfePvQ';
+CREATE DATABASE probe_lang_test_db_01 OWNER probe_lang_test_user_01;
+GRANT ALL PRIVILEGES ON DATABASE probe_lang_test_db_01 TO probe_lang_test_user_01;
